576857d7b27a6a1809eddc164e88e03312a9bfaf
Junior <jose@vert.se>
2021-01-01 22:09:28 -0300

    confiuguração inicial do layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Footer } from "./Components/Footer";
+import { Header } from "./Components/Header";
+import { Home } from "./Components/Home";
+import { Login } from "./Components/Login/Login";
 import "./App.css";
 
 function App() {
-  return <div className="App">Opa</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
