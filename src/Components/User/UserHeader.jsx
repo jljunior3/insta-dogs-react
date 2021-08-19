cb148530c721d9d06b808df018ea357a3ab5cca2
Junior <jose@vert.se>
2021-08-19 10:04:38 -0300

    add mobile menu

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -23,7 +23,7 @@ export const UserHeader = () => {
 
   return (
     <header className={styles.header}>
-      <h1 class="title">{title}</h1>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
   );
