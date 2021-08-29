d55293b7d066d9b412c2e0dc87275cba9b89ecc9
Junior <jose@vert.se>
2021-08-28 21:18:31 -0300

    add page not found

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Footer } from "./Components/Footer";
 import { Header } from "./Components/Header";
 import { Home } from "./Components/Home";
 import { Login } from "./Components/Login/Login";
-import "./App.css";
 import { UserStorage } from "./UserContext";
 import { User } from "./Components/User/User";
 import { ProtectedRoute } from "./Components/Helper/ProtectedRoute";
 import { Photo1 } from "./Components/Photo/Photo1";
+import "./App.css";
+import { UserProfile } from "./Components/User/UserProfile";
+import { NotFound } from "./Components/NotFound";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <ProtectedRoute path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo1 />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
