f8fa8cec633bfb3b4678b1a43f82e76ad43b8de0
Junior <jose@vert.se>
2021-09-05 21:40:41 -0300

    ajustes finais parte 1

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Feed } from "./Feed/Feed";
-import { Loading } from "./Helper/Loading";
+import Head from "./Helper/Head";
 
 export const Home = () => {
   return (
     <section className="container mainContainer">
+      <Head title="Fotos" description="Home do site dogs" />
       <Feed />
     </section>
   );
--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import Input from "./../Form/Input";
+import Button from "./../Form/Button";
+import { useForm } from "./../../Hooks/useForm";
+import { useFetch } from "./../../Hooks/useFetch";
+import { PASSWORD_LOST } from "./../../api";
+import { Error } from "./../Helper/Error";
 
 export const LoginPasswordLost = () => {
-  return <div>login password lost</div>;
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (login.validate) {
+      const { url, options } = PASSWORD_LOST({
+        login: login.value,
+        url: window.location.href.replace("perdeu", "resetar"),
+      });
+      const { json } = await request(url, options);
+    }
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        <p style={{ color: "#4c1" }}>{data}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="email" {...login} />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar email</Button>
+          )}
+        </form>
+      )}
+
+      <Error error={error} />
+    </section>
+  );
 };
